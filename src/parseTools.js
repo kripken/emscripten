b5e1824929d5731f37b4b569d0584031e2cc0cfd
Jukka Jylänki <jujjyl@gmail.com>
2021-04-14 15:57:14 +0300

    Address review

--- src/parseTools.js
+++ src/parseTools.js
@@ -1228,7 +1228,7 @@ function hasExportedFunction(func) {
 }
 
 // Returns true if LLVM produced the given function into compiled code as a Wasm export.
-function hasLlvmExportedFunction(func) {
+function hasLLVMExportedFunction(func) {
   return LLVM_EXPORTS.indexOf(func) != -1;
 }
 
